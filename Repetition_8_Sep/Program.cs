9172b236a486c84e3490a613952bc5834a33571d
HugoBror <90088173+HugoBror@users.noreply.github.com>
2022-09-08 23:41:14 +0200

    Tapetremsor

--- Repetition_8_Sep/Program.cs
+++ Repetition_8_Sep/Program.cs
@@ -1,7 +1,7 @@
 ﻿// ReSharper disable All
 // See https://aka.ms/new-console-template for more information
 
-/* 
+
 Console.WriteLine("Hej vad heter du?");
 string namn = "Hugo";
 Console.WriteLine("Jag heter " + namn);
@@ -12,7 +12,7 @@ Console.Write("Hej vad heter du? ");
 namn = Console.ReadLine();
 Console.WriteLine($"Hej {namn}");
 
-*/
+
 
 // Deklarera och tilldela värdet 0  
 
@@ -32,5 +32,30 @@ Console.WriteLine($"Du har levt {days} dagar i ditt liv. WOW!");
 
 // Övning Tapetremsor 
 
+int tapetrulle;
+int vagghojd;
+int vaggbredd;
+int tapetremsor;
+int tapetrullar;
+int tapetremsorRundat;
 
-Console.WriteLine("hej hopp");
+Console.WriteLine("Välkommen till TAPET RÄKNARE");
+Console.Write("Vad är längden på din tapetrullen? ");
+string tapetrulleS = Console.ReadLine();
+tapetrulle = Convert.ToInt32(tapetrulleS);
+
+Console.Write("Vad är din vägghöjd? ");
+string vagghojdS = Console.ReadLine();
+vagghojd = Convert.ToInt32(vagghojdS);
+
+tapetremsor = (tapetrulle / vagghojd);
+
+Console.WriteLine($"Då får du alltså {tapetremsor}st tapetremsor från din tapetrulle");
+
+Console.Write("Vad är bredden på din vägg? ");
+string vaggbreddS = Console.ReadLine();
+vaggbredd = Convert.ToInt32(vaggbreddS);
+
+tapetrullar = vaggbredd / tapetremsor;
+
+Console.WriteLine($"Du behöver alltså {tapetrullar}st tapetrullar för att kunna tapetsera din vägg!");
